4ba3c48c3f2fc5a0871fbf32595fad7af6500e82
Michal Bojanowski <michal2992@gmail.com>
2017-12-03 14:50:07 +0100

    Expecting warnings

--- tests/testthat/test-extract_codings.R
+++ tests/testthat/test-extract_codings.R
@@ -2,7 +2,7 @@ context("Testing extract_codings()")
 
 test_that("it works", {
   fname <- system.file(file.path("exdata", "test.mex"), package="maxqdar")
-  expect_silent(
+  expect_warning(
     extract_codings(fname)
   )
 })

fd0aa742e29f16a92027a9a929c67ae412eafe3f
Michal Bojanowski <michal2992@gmail.com>
2019-10-28 17:22:33 +0100

    Hacking

--- tests/testthat/notes.R
+++ tests/testthat/notes.R
@@ -51,3 +51,24 @@ test_that("Reading XLS works", {
 
   d <- readxl::read_xls("inst/exdata/test.xls")
 })
+
+
+
+
+library(dplyr)
+db <- DBI::dbConnect(RSQLite::SQLite(), "inst/exdata/test.mex")
+
+db_list_tables(db)
+
+texts <-
+  db %>%
+  tbl("Texts_PlainTextWin_BLOBS") %>%
+  collect()
+
+r <- texts$Value[[2]]
+rawToChar(r)
+
+memDecompress( r[-(1:5)], "unknown")
+
+ch <- " 229-  1722, Maio, 28, Lisboa AVISO do secretário de estado, Diogo de Mendonça Corte Real ao governador do Rio de Janeiro, Aires de Saldanha de Albuquerque, ordenando que se dêem os despachos necessários sobre a carta do ouvidor-geral do Rio das Velhas, José de Sousa Valdez e sobre as razões por que soltou os soldados sem serem castigados pelo que fizeram contra o capitão-mor de Vila Real, Lucas Ribeiro de Almeida. Anexo: escrito, carta. Nº de inventário no catálogo: 227. AHU-Minas Gerais, cx. 3, doc. 22. AHU_CU_011, Cx. 3, D. 229."
+charToRaw(ch)
